4855888ad815a6799b9197c94c6b84d599b66379
Kiril Shchasny <kiril3467@yandex.ru>
2017-08-08 15:04:31 +0300

    Add product service

--- Task/Program.cs
+++ Task/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Task.Entities;
+using Task.Services;
 
 namespace Task
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void TestWithoutService()
         {
             using (var db = new ShopContext())
             {
@@ -50,11 +52,11 @@ namespace Task
                     Title = "Nike ball"
                 };
 
-                var order1 = new Order {Product = phone, Quantity = 1};
+                var order1 = new Order { Product = phone, Quantity = 1 };
                 var order2 = new Order { Product = ball, Quantity = 2 };
-                var order3 = new Order {Product = auto, Quantity = 3};
-                var order4 = new Order {Product = window, Quantity = 10};
-                var purchase = new Purchase{Basket = new List<Order>{order1, order2, order3, order4}, PurchaseId = 1};
+                var order3 = new Order { Product = auto, Quantity = 3 };
+                var order4 = new Order { Product = window, Quantity = 10 };
+                var purchase = new Purchase { Basket = new List<Order> { order1, order2, order3, order4 }, PurchaseId = 1 };
 
                 db.Products.Add(phone);
                 db.Products.Add(auto);
@@ -64,8 +66,8 @@ namespace Task
                 db.SaveChanges();
 
                 var prodQuery = from p in db.Products
-                            orderby p.Title
-                            select p;
+                                orderby p.Title
+                                select p;
 
                 var purchaseQ = from p in db.Purchases
                                 select p;
@@ -84,6 +86,66 @@ namespace Task
 
                 Console.ReadKey();
             }
+        }
+
+        static void TestWithService()
+        {
+            Console.WriteLine("Start work with DB: ");
+            var phone = new Product
+            {
+                Category = Category.Electronics,
+                Description = "New mobile phone",
+                Image = null,
+                Price = 350,
+                Title = "iPhone blabla"
+            };
+
+            var auto = new Product
+            {
+                Category = Category.AutoMoto,
+                Description = "New auto",
+                Image = null,
+                Price = 5000,
+                Title = "Audi"
+            };
+
+            var window = new Product
+            {
+                Category = Category.BuildingAndRepair,
+                Description = "New euro window",
+                Image = null,
+                Price = 100,
+                Title = "EuroWindowSuperCool"
+            };
+
+            var ball = new Product
+            {
+                Category = Category.Sport,
+                Description = "New football ball",
+                Image = null,
+                Price = 15,
+                Title = "Nike ball"
+            };
+
+            var service = new ProductService(new ShopContext());
+            service.Create(phone);
+            //service.Create(phone); throws exception
+            service.Create(auto);
+            service.Create(window);
+            service.Create(ball);
+
+            foreach (var item in service.GetAll())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            //TestWithoutService();
+            //TestWithService();
         }
     }
 }
---
+++ Task/Services/ProductService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task.Entities;
+
+namespace Task.Services
+{
+    public class ProductService
+    {
+        private DbContext _context;
+
+        public ProductService(DbContext context) => _context = context;
+
+        public Product GetProductEntity(string title)
+        {
+            return _context.Set<Product>().FirstOrDefault(x => x.Title == title);
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            var all = _context.Set<Product>();
+            return all;
+        }
+
+        public void Create(Product product)
+        {
+            if (_context.Set<Product>().FirstOrDefault(x => x.Title == product.Title) != null)
+                throw new InvalidOperationException($"Product {product.Title} exist in this storage");
+
+            _context.Set<Product>().Add(product);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Product product)
+        {
+            var deletedProd = _context.Set<Product>().SingleOrDefault(x => x.Title == product.Title);
+            if(deletedProd == null)
+                throw new InvalidOperationException($"Product {product.Title} doesn't exist");
+
+            _context.Set<Product>().Remove(deletedProd);
+            _context.SaveChanges();
+        }
+
+        public void Update(Product product)
+        {
+            //?
+            throw new NotImplementedException();
+        }
+    }
+}
